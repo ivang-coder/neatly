6bdda3a3e5aadff62cd278d81580ed7430632113
ivang-coder <ui@localhost.local>
2021-12-04 13:06:54 -0500

    Change log created, readme updated

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # rexifier
 rEXIFier is a Shell-written EXIF-based media file processor with sort, dedup and other features
+
+# This script is used to sort media files (jpg, jpeg, mp4, mpg and mov ). 
+#  It uses EXIFTOOL to extract "SubSecCreateDate", "CreateDate" and 
+#  "Model" metadata. If EXIF attempts fail, then less reliable file attributes get
+#  extracted as the last resort. In case the file attribute 
+#  extaction fails, the source filename gets amended by adding
+#  "UNVERIFIED-" prefix
+#
+# File Processing Overview
+#
+# Step 1. Search media files in Source Directory and 
+# SourceDirectory/
+#   file01.jpg
+#   file02.jpeg
+#   file03.mov
+#   file04.jpeg
+#   file05.mov
+#
+# Step 2. Create temporary Work-In-Progress (WIP) directory
+# SourceDirectory/WIP-YYYYMMDD-HHmmss
+#
+# Step 3. Move the found files to temporary Work-In-Progress (WIP) directory
+#
+# SourceDirectory/WIP-YYYYMMDD-HHmmss
+#   file01.jpg
+#   file02.jpeg
+#   file03.mov
+#   file04.jpeg
+#   file05.mov
+#
+# Step 4. Create directories:
+#         FileUnverified directory for files with corrupted/unreadable metadata
+#         FileNameDuplicates directory for by-name duplicates between Work-In-Progress and Destination Directory, i.e. 
+#           files with matching names and non-matching content
+#         FileContentDuplicates directory for by-content duplicates in the Destination Directory, i.e. 
+#           files with non-matching names and matching content
+#
+# DestinationDirectory/FileUnverified
+# DestinationDirectory/FileNameDuplicates
+# DestinationDirectory/FileContentDuplicates
+#
+# Step 5. Prepare and move the WIP files to DestinationDirectory with YYYY/MM/DD structure
+#
+# DestinationDirectory/
+#   FileUnverified/
+#     file01.jpg
+#   FileNameDuplicates/
+#     file02.jpeg
+#   FileContentDuplicates/
+#   2007/
+#       05/
+#         01/
+#           file03.mov
+#   2009/
+#       12/
+#         29/
+#           file04.jpeg
+#   2015/
+#       01/
+#         17/
+#           file05.mov
+#
+# Credits for tips and inspiration
+# https://stackoverflow.com/questions/32062159/how-retrieve-the-creation-date-of-photos-with-a-script
--- rEXIFier.sh
+++ rEXIFier.sh
@@ -8,79 +8,6 @@
 # "Unverified" file deduplication
 # 
 
-# This script is used to sort media files (jpg, jpeg, mp4, mpg and mov ). 
-#  It uses EXIFTOOL to extract "SubSecCreateDate", "CreateDate" and 
-#  "Model" metadata. If EXIF attempts fail, then less reliable file attributes get
-#  extracted as the last resort. In case the file attribute 
-#  extaction fails, the source filename gets amended by adding
-#  "UNVERIFIED-" prefix
-#
-# File Processing Overview
-#
-# Step 1. Search media files in Source Directory and 
-# SourceDirectory/
-#   file01.jpg
-#   file02.jpeg
-#   file03.mov
-#   file04.jpeg
-#   file05.mov
-#
-# Step 2. Create temporary Work-In-Progress (WIP) directory
-# SourceDirectory/WIP-YYYYMMDD-HHmmss
-#
-# Step 3. Move the found files to temporary Work-In-Progress (WIP) directory
-#
-# SourceDirectory/WIP-YYYYMMDD-HHmmss
-#   file01.jpg
-#   file02.jpeg
-#   file03.mov
-#   file04.jpeg
-#   file05.mov
-#
-# Step 4. Create directories:
-#         FileUnverified directory for files with corrupted/unreadable metadata
-#         FileNameDuplicates directory for by-name duplicates between Work-In-Progress and Destination Directory, i.e. 
-#           files with matching names and non-matching content
-#         FileContentDuplicates directory for by-content duplicates in the Destination Directory, i.e. 
-#           files with non-matching names and matching content
-#
-# DestinationDirectory/FileUnverified
-# DestinationDirectory/FileNameDuplicates
-# DestinationDirectory/FileContentDuplicates
-#
-# Step 5. Prepare and move the WIP files to DestinationDirectory with YYYY/MM/DD structure
-#
-# DestinationDirectory/
-#   FileUnverified/
-#     file01.jpg
-#   FileNameDuplicates/
-#     file02.jpeg
-#   FileContentDuplicates/
-#   2007/
-#       05/
-#         01/
-#           file03.mov
-#   2009/
-#       12/
-#         29/
-#           file04.jpeg
-#   2015/
-#       01/
-#         17/
-#           file05.mov
-#
-# Credits for tips and inspiration
-# https://stackoverflow.com/questions/32062159/how-retrieve-the-creation-date-of-photos-with-a-script
-#
-#=======================================================================
-#   Changelog :
-#   20200520 v0.1: initial version with simple EXIF extraction
-#   20200607 v0.5: EXIF SubSecCreateDate and CreateDate extractions added
-#   20200723 v0.9: EXIF Model and File System ModifyTime extractions added
-#   20200816 v1.0: alpha-version of Minimal Viable Product with file duplication avoidance
-#=======================================================================
-
-
 #=======================================================================
 # Default parameters
 #=======================================================================
@@ -301,13 +228,13 @@ InstanceCount=$(ps -ef | grep $InstanceNameBase | grep -v grep | wc -l)
 # with the name matching the script name, hence we're checking if there are
 # more than 2 instances
 if [[ ${InstanceCount} > 2 ]]; then
-  printf "It appears more than one $InstanceName instance is running\n"
+  printf "It appears more than one $InstanceName instance is running. Exiting now.\n"
   exit
 fi
 
 # Checking if application or service is installed, piping errors to NULL
 if ( ! command -v exiftool &> /dev/null ) ; then
-  printf "It appears 'exiftool' is not installed or it could not be found\n"
+  printf "It appears 'exiftool' is not installed or it could not be found. Exiting now.\n"
   exit
 else
   EXIFTOOLVER=$(exiftool -ver)
@@ -315,7 +242,7 @@ fi
 
 # Checking minimal requirements for the arguments, expecting at least one argument
 if [[ ${#} -lt 1 ]] ; then
-  printf "At least one argument is expected, exiting\n"
+  printf "At least one argument is expected. Exiting now.\n"
   printf "${HelpTip}"
   exit
 fi
@@ -331,7 +258,7 @@ case "${1}" in
   /*|.) 
     # Exiting if there are issues with the source path
     if [[ ! -e ${1} ]] ; then
-      printf "Source path ${1} could not be found or does not exist, exiting\n"
+      printf "Source path ${1} could not be found or does not exist. Exiting now.\n"
       printf "${SourcePathTip}\n"
       exit
     else
@@ -341,7 +268,7 @@ case "${1}" in
     ;;
   # Exiting if no expected parameter found
   *) 
-    echo "Mandatory source path is invalid or could not be identified: ${1}, exiting\n"
+    echo "Mandatory source path is invalid or could not be identified: ${1}. Exiting now.\n"
     printf "${UsageTip}${SourcePathTip}"
     exit
     ;;
@@ -353,7 +280,7 @@ case "${2}" in
   /*|.) 
     # Exiting if there are issues with the destination path
     if [[ ! -e ${2} ]] ; then
-      printf "Destination path ${2} could not be found or does not exist, exiting\n"
+      printf "Destination path ${2} could not be found or does not exist. Exiting now.\n"
       printf "${DestinationPathTip}\n"
       exit
     else
@@ -363,7 +290,7 @@ case "${2}" in
     ;;
   # Exiting if no expected parameter found
   *) 
-    echo "Mandatory destination path is invalid or could not be identified: ${2}, exiting\n"
+    echo "Mandatory destination path is invalid or could not be identified: ${2}. Exiting now.\n"
     printf "${UsageTip}${DestinationPathTip}"
     exit
     ;;
@@ -430,7 +357,7 @@ if [[ ${#} -gt 2 ]] ; then
   done
 # Applying default parameters if the required arguments have not been passed
 else
-  printf "No optional parameters have been passed, applying defaults\n"
+  printf "No optional parameters have been passed. Applying the defaults.\n"
 fi
 
 printf "Proceeding with parameters below.\n" 
